c97a2b9bad160070389027f35be06d593d00d811
Benjamin Simon <b3nshi@Benjamins-MacBook-Pro.local>
2017-04-23 09:32:05 -0400

    Images and styling for posts

--- js/app.js
+++ js/app.js
@@ -6,10 +6,34 @@
 
     vm.lang = 'en';
     vm.category = '';
+    vm.modalPost = {};
+    vm.showModal = false;
+
+    vm.translations = {
+      en: {
+        readMore: 'Read more'
+      },
+      es: {
+        readMore: 'Leer más'
+      }
+    };
 
     $http.get('data/posts.json')
        .then(function(res) {
          vm.posts = res.data.posts;
         });
+
+    vm.openPost = function (post) {
+      $http.get('data/' + post + '.json')
+       .then(function(res) {
+         vm.modalPost = res.data;
+         vm.showModal = true;
+        });
+    };
   }]);
+
+  app.filter('renderHtml', ['$sce', function ($sce) {
+    return $sce.trustAsHtml;
+  }]);
+
 }(angular));
